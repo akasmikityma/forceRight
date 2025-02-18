10a96393574d9772dda6a7ef48149c97ba9005bf
bishal maity <bishalmaity90@gmail.com>
2025-02-18 15:01:18 +0530

    removed dist and prisma from tracking

--- forceRight_1/prisma/migrations/20250207154247_updated_tags/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `tag` column on the `Problem` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Problem" DROP COLUMN "tag",
-ADD COLUMN     "tag" TEXT[];
--- forceRight_1/prisma/migrations/20250207154351_updated_tags_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `tag` on the `Problem` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Problem" DROP COLUMN "tag",
-ADD COLUMN     "tags" TEXT[];
